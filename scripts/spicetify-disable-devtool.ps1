de7afc9df79767bd31824450c9b40809001f0982
TheRandomLabs <therandomlabsinc@gmail.com>
2020-05-25 16:45:18 +1000

    Improve scripts

--- scripts/spicetify-disable-devtool.ps1
+++ scripts/spicetify-disable-devtool.ps1
@@ -7,13 +7,11 @@ $spotify_running = Get-Process -ErrorAction Ignore -Name Spotify
 Stop-Process -ErrorAction Ignore -Name Spotify
 
 # restore should be run separately because if there is no backup, it will error and exit
-if ($quiet) {
-    & "$PSScriptRoot\spicetify.exe" restore --quiet --no-restart
-    & "$PSScriptRoot\spicetify.exe" backup apply disable-devtool --quiet --no-restart
-} else {
-    & "$PSScriptRoot\spicetify.exe" restore --no-restart
-    & "$PSScriptRoot\spicetify.exe" backup apply disable-devtool --no-restart
-}
+# This must be run with --quiet to prevent the version mismatch warning
+& "$PSScriptRoot\spicetify.exe" restore --quiet --no-restart
+
+if ($quiet) { & "$PSScriptRoot\spicetify.exe" backup apply disable-devtool --quiet --no-restart }
+else { & "$PSScriptRoot\spicetify.exe" backup apply disable-devtool --no-restart }
 
 if (Get-Command "blockthespot" -ErrorAction Ignore) { blockthespot }
 if ($spotify_running) { Start-Process "$(& `"$PSScriptRoot\get-spotify-path.ps1`")" }
