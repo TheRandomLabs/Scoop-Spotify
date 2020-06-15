e735f583003e221e3b431a13547f44794f578321
TheRandomLabs <therandomlabsinc@gmail.com>
2020-06-15 21:14:23 +1000

    Update basic-setup.ps1

--- basic-setup.ps1
+++ basic-setup.ps1
@@ -3,7 +3,7 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.
 scoop install git
 
 scoop bucket add spotify https://github.com/TheRandomLabs/Scoop-Spotify.git
-scoop install spotify-latest blockthespot spicetify-cli spicetify-themes genius-spicetify spicetify-autovolume
+scoop install spotify-latest blockthespot spicetify-cli spicetify-themes genius-spicetify
 
 spicetify config current_theme Adapta-Nokto --quiet
 spicetify-enable-devtool -quiet
